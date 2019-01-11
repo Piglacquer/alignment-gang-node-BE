09575fb895de9ce581873c1d6722a04143533159
Patrick Biffle <pbiffle@gmail.com>
2019-01-11 14:38:04 -0700

    added port env variable in appjs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const markersRoutes = require('./routes/markers')
 const locationsRoutes = require('./routes/locations')
 const app = express()
-const port = 3001
+const port = process.env.PORT || 3001
 
 app.use(bodyParser.json())
 app.use(cors())
